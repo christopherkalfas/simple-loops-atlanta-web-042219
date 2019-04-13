e7a5dff7aecfacbd5cda6e6d23cfc434bc7b15cc
Christopher Kalfas <chriskalfas@yahoo.com>
2019-04-13 17:54:03 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -13,10 +13,11 @@ def loop_iterator(number_of_times)
 end
 
 def times_iterator(number_of_times)
+  phrase = "Welcome to Flatiron School's Web Development Course!"
   7.times do 
     puts phrase
   end 
-  phrase = "Welcome to Flatiron School's Web Development Course!"
+  
   
 end
 
